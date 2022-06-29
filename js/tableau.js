063b8556eaa65adbdc742753151e8923e14c8183
Lillian Li <61984727+usual616@users.noreply.github.com>
2022-06-30 00:09:07 +0800

    Update tableau.js

--- js/tableau.js
+++ js/tableau.js
@@ -1,6 +1,6 @@
 function initViz() {
     var containerDiv = document.getElementById("tableauViz"),
-    url = "https://public.tableau.com/views/_16549482335250/sheet4_1?:language=zh-TW&:display_count=n&:origin=viz_share_link";
+    url = "https://public.tableau.com/views/_16565184763270/sheet4?:language=zh-TW&publish=yes&:display_count=n&:origin=viz_share_link";
 
     var viz = new tableau.Viz(containerDiv, url);
 }
